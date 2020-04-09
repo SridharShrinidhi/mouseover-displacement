2552cfdfa1ee432c08750c9b26e183fceecc9e40
Shrinidhi Sridhar <srid0002@algonquinlive.com>
2020-04-08 21:49:20 -0400

    Update the styling

--- js/main.js
+++ js/main.js
@@ -3,5 +3,7 @@ new hoverEffect ({
   intensity: .2,
   image1: 'images/image-1.jpg',
   image2: 'images/image-2.jpg',
+  speedIn: 2,
+  speedOut: 2,
   displacementImage: 'images/hover-image.png',
 });
