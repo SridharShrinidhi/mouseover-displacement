dfbc3a99be8be223d68171b006da668efc144e3f
Shrinidhi Sridhar <srid0002@algonquinlive.com>
2020-04-08 20:03:28 -0400

    Update the file

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 new hoverEffect ({
   parent: document.querySelector('.hover'),
   intensity: .2,
-  image1: '/images/image-1.jpg',
-  image2: '/images/image-2.jpg',
-  displacementImage: '/images/hover-image.png',
+  image1: './image-1.jpg',
+  image2: './image-2.jpg',
+  displacementImage: './hover-image.png',
 });
